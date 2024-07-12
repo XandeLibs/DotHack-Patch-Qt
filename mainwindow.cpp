1dc5ce0e02b6ee3280714866fa9e2f5a09cebf6a
Alexandre Libório <alexandre_liborio@outlook.com>
2024-07-12 16:39:27 -0400

    Initial commit

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,7 @@
 #include "./ui_mainwindow.h"
 #include <QFileDialog>
 #include <QMessageBox>
+#include <fstream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -24,38 +25,85 @@ void MainWindow::on_TB_ChooseFolder_clicked()
 
     using namespace std::filesystem;
 
-    if (!(exists(Temp_FolderPath/strHack1) ||
-          exists(Temp_FolderPath/strHack2) ||
-          exists(Temp_FolderPath/strHack3) ||
-          exists(Temp_FolderPath/strHack4))){
+    if (!(exists(Temp_FolderPath/c_strHack1) ||
+          exists(Temp_FolderPath/c_strHack2) ||
+          exists(Temp_FolderPath/c_strHack3) ||
+          exists(Temp_FolderPath/c_strHack4))){
         QMessageBox::warning(this, tr("Error"), (tr("Game folder must have the following files: \n")) +
-                                                 QString::fromStdString(strHack1) + '\n' +
-                                                 QString::fromStdString(strHack2) + '\n' +
-                                                 QString::fromStdString(strHack3) + '\n' +
-                                                 QString::fromStdString(strHack4) + '\n');
+                                                 QString::fromStdString(c_strHack1) + '\n' +
+                                                 QString::fromStdString(c_strHack2) + '\n' +
+                                                 QString::fromStdString(c_strHack3) + '\n' +
+                                                 QString::fromStdString(c_strHack4) + '\n');
         return;
     }
 
     setFolderPath(Temp_FolderPath);
 }
 
+void MainWindow::EnablePatches(){
+    ui->GB_DmgScaling->setEnabled(true);
+    ui->GB_Exp->setEnabled(true);
+    ui->TB_Apply->setEnabled(true);
+}
+
 void MainWindow::setFolderPath(std::filesystem::path Path){
     FolderPath = Path;
     ui->LE_FolderPath->setText(QString::fromStdString(FolderPath.string()));
+    EnablePatches();
     ReadAppliedPatches();
 }
 
 void MainWindow::initialFolderCheck(){
     std::filesystem::path defWinPath = "C:/Program Files (x86)/Steam/steamapps/common/hackGU";
 
-    if ((exists(defWinPath/strHack1) ||
-          exists(defWinPath/strHack2) ||
-          exists(defWinPath/strHack3) ||
-          exists(defWinPath/strHack4))){
+    if ((exists(defWinPath/c_strHack1) ||
+          exists(defWinPath/c_strHack2) ||
+          exists(defWinPath/c_strHack3) ||
+          exists(defWinPath/c_strHack4))){
         setFolderPath(defWinPath);
     }
 }
 
 void MainWindow::ReadAppliedPatches(){
+    auto dll1_filePath = QDir::toNativeSeparators(QString::fromStdString((FolderPath/c_strHack1).generic_string()));
+    std::fstream dll1(dll1_filePath.toStdString());
+
+    dll1.seekg(lvlScaleAddress[0]);
+    auto byte = dll1.get();
+
+    if(byte == lvlScaleInstructions[0]){
+        ui->RB_DSEnabled->setChecked(true);
+    }
+    else{
+        ui->RB_DSEnabled->setChecked(false);
+    }
+
+    dll1.seekg(xpAddress[0]);
+    byte = dll1.get();
+
+    switch (byte) {
+    case xpValuesOriginal[0]:
+        ui->RB_EXPOriginal->setChecked(true);
+        break;
+    case (int)((double)xpValuesOriginal[0] * 1.3):
+        ui->RB_EXPLastRecode->setChecked(true);
+        break;
+    default:
+        ui->RB_EXPCustom->setChecked(true);
+        ui->LE_EXPPercent->setText(QString::number(byte / xpValuesOriginal[0] * 100));
+        break;
+    }
+}
+
+void MainWindow::on_TB_Apply_clicked()
+{
+    if (!(ui->RB_DSDisabled->isChecked() || ui->RB_DSEnabled->isChecked())){
+        return;
+    }
+    if (!(ui->RB_EXPOriginal->isChecked() || ui->RB_EXPLastRecode->isChecked() || ui->RB_EXPCustom->isChecked())){
+        return;
+    }
+
 
 }
+
--- mainwindow.h
+++ mainwindow.h
@@ -23,16 +23,25 @@ public:
 private slots:
     void on_TB_ChooseFolder_clicked();
 
+    void on_TB_Apply_clicked();
+
 private:
     Ui::MainWindow *ui;
-    const std::string strHack1 = "hackGU_vol1.dll";
-    const std::string strHack2 = "hackGU_vol2.dll";
-    const std::string strHack3 = "hackGU_vol3.dll";
-    const std::string strHack4 = "hackGU_vol4.dll";
+    static constexpr auto c_strHack1 = "hackGU_vol1.dll";
+    static constexpr auto c_strHack2 = "hackGU_vol2.dll";
+    static constexpr auto c_strHack3 = "hackGU_vol3.dll";
+    static constexpr auto c_strHack4 = "hackGU_vol4.dll";
+    static constexpr std::array xpAddress = {0x7d4cc0, 0x8fb2d0, 0x9fa140, 0x9d9260};
+    static constexpr std::array xpValuesOriginal = {0x96, 0x91, 0x8c, 0x87, 0x82, 0x7d, 0x78, 0x73, 0x64, 0x55, 0x4b, 0x41, 0x2d, 0x14, 0x0a, 0x07, 0x05, 0x04, 0x03};
+    static constexpr std::array lvlScaleInstructions = {0xF3, 0x41, 0x0F, 0x59, 0xD8};
+    static constexpr std::array lvlScaleAddress = {0x48f695, 0x56b5f5, 0x5744c1, 0x557cb1};
+    static constexpr std::array noop = {0x90, 0x90, 0x90, 0x90, 0x90};
     std::filesystem::path FolderPath;
 
     void initialFolderCheck();
+    void EnablePatches();
     void ReadAppliedPatches();
+    void ApplyPatches();
     Q_PROPERTY(std::filesystem::path FolderPath WRITE setFolderPath FINAL)
 };
 #endif // MAINWINDOW_H
